504be8218d7c31695b0e227bc528e4db9e38c47b
各務 将士 <(null)>
2015-07-06 16:15:49 +0900

    remove cache

--- Build/Intermediates/DistributionDriver.build/Debug-iphoneos/DistributionDriver.build/Objects-normal/arm64/MDPinCollectionCell.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Mercy/Works/Modelor/Distribution/repo_driver/DistributionDriver/MDView/MDPinCollectionCell.m \
-  /Users/Mercy/Works/Modelor/Distribution/repo_driver/DistributionDriver/MDView/MDPinCollectionCell.h
